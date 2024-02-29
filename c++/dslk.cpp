0e12138ab9ee289b788fffd970a3b37435b9014e
minhvu93258 <quangminh9303@gmail.com>
2024-03-01 03:04:12 +0700

    test

--- c++/dslk.cpp
+++ c++/dslk.cpp
@@ -1,32 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
+struct node{
     int data;
-    Node* next;
+    node *next;
+    node * prev;
 };
-int main (){
-    Node* head =NULL;
-    Node* second= NULL;
-    Node* third= NULL;
-
-    head= new Node();
-    second =new Node();
-    third = new Node();
-
-    head-> data=1;
-    head-> next= second;
-
-    second->data=2;
-    second->next= third;
-
-    third-> data=3;
-    third-> next=NULL;
-
-Node* current= head;
-while(current!=NULL){
-    cout<< current->data<<" ";
-    current= current->next;
+node *makeNode(int x){
+    node *newNode= new node;
+    newNode->data=x;
+    newNode->next= NULL;
+    newNode->prev= NULL;
+    return newNode;
 }
+void duyet(node *head){
+    while(head!=NULL){
+        cout<<head->data<<' ';
+        head= head->next;
+    }
+    cout<<endl;
+}
+void themdau(node **head,int x){
+    node *newNode= makeNode(x);
+    newNode->next = (*head);
+    if(*head != NULL)
+    (*head)->prev = newNode;
+    (#head) = newNode;
+}
+void themcuoi(node **head, int x){
+node *newNode= makeNode(x);
+if(*head==NULL){
+    *head= newNode;
+    return ;
+}
+node *temp = *head;
+while(temp->next!=NULL){
+    temp=temp->next;
+}
+temp->next= newNode;
+newNode->prev=temp;
+}
+int main (){
+    
+
 return 0;
 }
